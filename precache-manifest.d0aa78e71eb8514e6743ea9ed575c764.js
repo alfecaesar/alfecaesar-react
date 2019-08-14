e846192388c415d08244c871c32e2cd50f9d7f1b
alfecaesar <alfecaesar@gmail.com>
2019-08-14 08:43:06 +0800

    Updates

---
+++ precache-manifest.d0aa78e71eb8514e6743ea9ed575c764.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0694c701dd1a8ec581ba9170970659de",
+    "url": "/alfecaesar-react/index.html"
+  },
+  {
+    "revision": "4f5096cc6ac5a6aae2f4",
+    "url": "/alfecaesar-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/alfecaesar-react/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "4f5096cc6ac5a6aae2f4",
+    "url": "/alfecaesar-react/static/js/main.3c2f127d.chunk.js"
+  },
+  {
+    "revision": "c432833d245a59c572a0",
+    "url": "/alfecaesar-react/static/js/runtime~main.833906cc.js"
+  }
+]);
